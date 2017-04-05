d4962aad58631282f2c9d2ae3a20c2b65ece5494
Jaksa Vuckovic <jaksa.vuckovic@zuehlke.com>
2017-04-05 11:49:40 +0200

    added test for the readme file

--- src/test/java/me/jaksa/UnreliableTest.java
+++ src/test/java/me/jaksa/UnreliableTest.java
@@ -88,4 +88,29 @@ public class UnreliableTest {
         Assert.assertTrue(service.success);
         Assert.assertEquals(100, service.tries);
     }
+
+    @Test
+    public void testExample() throws Exception {
+        UnreliableService unreliableService = new UnreliableService();
+
+        // this will try to invoke the service 5 times
+        Unreliable.tenaciusly(() -> unreliableService.doSomething(), 5);
+
+        // the default number of tries is 3
+        Unreliable.tenaciusly(() -> unreliableService.doSomething());
+
+        // you can also retrieve values
+        String result = Unreliable.tenaciusly(() -> unreliableService.getSomething());
+        System.out.println(result);
+
+        // if after X times it still fails, a RuntimeException will be thrown
+        try {
+            Unreliable.tenaciusly(() -> unreliableService.doSomething(), 2);
+        } catch (RuntimeException e) {
+            System.out.println("I got tired of trying. Last exception: " + e.getCause().getMessage());
+        }
+
+        // or keep trying an infinite number of times
+        Unreliable.keepTrying(() -> unreliableService.doSomething());
+    }
 }
